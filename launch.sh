84d653b341e7b63ec316384c333a7afb5fca421c
Gaetan Morand <gaetan.morand@umontpellier.fr>
2025-02-19 17:11:34 +0100

    Added files needed for marbec-gpu run

---
+++ launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --partition=standard
+#SBATCH --job-name=malpolon_rls
+#SBATCH --mem=32G
+#SBATCH --gres=gpu:1
+#SBATCH --dependency=afterany:5159
+#SBATCH -o malpolon_rls.out
+#SBATCH -e malpolon_rls.err
+
+
+source /home/gmorand/venvs/deepsdm/bin/activate
+python rls_aus_binned.py 
