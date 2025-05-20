727c1e7e40d71753cfce1248725a29904cbe5a6a
Gaetan Morand <gaetan.morand@umontpellier.fr>
2025-05-20 16:56:38 +0200

    modified:   config/rls_aus_binned.yaml 	modified:   config/rls_aus_reg.yaml 	modified:   ../../malpolon/data/data_module.py

--- malpolon/data/data_module.py
+++ malpolon/data/data_module.py
@@ -292,36 +292,38 @@ class BaseDataModule(pl.LightningDataModule, ABC):
 
 def load_modality(survey_id, inputs_path, modality):
 
-    match modality:
-        case "env":
-            filename = Path(inputs_path) / "env" / (survey_id + '.npy')
-            x = np.load(filename).astype(np.float32)
-            return torch.from_numpy(np.transpose(x, (2, 0, 1)))
-        case "hum":
-            filename = Path(inputs_path) / "hum" / (survey_id + '.npy')
-            x = np.load(filename).astype(np.float32)
-            return torch.from_numpy(np.transpose(x, (2, 0, 1)))
-        case "sat":
-            filename = Path(inputs_path) / "sat" / (survey_id + '.jpg')
-            if filename.exists():
-                with Image.open(filename) as rgb_patch:
-                    return v2.functional.pil_to_tensor(rgb_patch).float() / 255
-            else:
-                return torch.zeros([3, 995, 995]) 
-        case "dhw":
-            filename = Path(inputs_path) / "dhw" / (survey_id + '.npy')
-            x = np.load(filename).astype(np.float32)
-            return torch.unsqueeze(torch.from_numpy(x),0)
-        case "best10":
-            filename = Path(inputs_path) / "best10" / (survey_id + '.npy')
-            x = np.load(filename).astype(np.float32)
-            return torch.from_numpy(x)
-        case "timeseries":
-            pass
-        case "envhum":
-            env = load_modality(survey_id, inputs_path, "env")
-            hum = load_modality(survey_id, inputs_path, "hum")
-            return torch.from_numpy(np.concatenate([env, hum], axis=0))
+    if modality == 'sat':
+        filename = Path(inputs_path) / "sat" / (survey_id + '.jpg')
+        if filename.exists():
+            with Image.open(filename) as rgb_patch:
+                return v2.functional.pil_to_tensor(rgb_patch).float() / 255
+        else:
+            return torch.zeros([3, 995, 995]) 
+                
+    elif modality == 'envhum':
+        env = load_modality(survey_id, inputs_path, "env")
+        hum = load_modality(survey_id, inputs_path, "hum")
+        return torch.from_numpy(np.concatenate([env, hum], axis=0))
+        
+    elif modality in ('env', 'hum'):
+        # 3D cubes
+        filename = Path(inputs_path) / modality / (survey_id + '.npy')
+        x = np.load(filename).astype(np.float32)
+        return torch.from_numpy(np.transpose(x, (2, 0, 1)))
+
+    elif modality == 'dhw':
+        # 2D data
+        filename = Path(inputs_path) / "dhw" / (survey_id + '.npy')
+        x = np.load(filename).astype(np.float32)
+        return torch.unsqueeze(torch.from_numpy(x),0)
+        
+    else:
+        # 1D data
+        filename = Path(inputs_path) / modality / (survey_id + '.npy')
+        x = np.load(filename).astype(np.float32)
+        return torch.from_numpy(x)
+
+            
 
 
 def load_patch(
