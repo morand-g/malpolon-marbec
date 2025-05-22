e2bc776ff7bd3870178e7de56647bcbf58fb9be1
sarah-marbec <sarah.kiati@umontpellier.fr>
2025-05-22 14:42:47 +0200

    adding popdense data module

--- malpolon/data/data_module.py
+++ malpolon/data/data_module.py
@@ -28,6 +28,9 @@ from torch.utils.data import DataLoader, Dataset
 
 from torchvision.transforms import v2
 
+from torchgeo.datamodules import GeoDataModule
+from torchgeo.samplers import RandomBatchGeoSampler, GridGeoSampler
+
 
 if TYPE_CHECKING:
     from typing import Any, Callable, Optional, Union
@@ -645,3 +648,162 @@ class RLSDataModule(BaseDataModule):
         weights /= weights.sum()
 
         return weights
+
+
+class PopDensGeoDataModule(GeoDataModule):
+
+    def get_dataset(self, split: str, transform: Callable, **kwargs: Any) -> Dataset:
+        """Return the dataset corresponding to the split.
+
+        Parameters
+        ----------
+        split : str
+            Type of dataset. Values must be on of ["train", "val",
+            "test"]
+        transform : Callable
+            data transforms to apply when loading the dataset
+
+        Returns
+        -------
+        Dataset
+            dataset corresponding to the split
+        """
+
+    def get_train_dataset(self) -> Dataset:
+        """Call self.get_dataset to return the train dataset.
+
+        Returns
+        -------
+        Dataset
+            train dataset
+        """
+        dataset = self.get_dataset(
+            split="train",
+            transform=self.train_transform,
+        )
+        return dataset
+
+    def get_val_dataset(self) -> Dataset:
+        """Call self.get_dataset to return the validation dataset.
+
+        Returns
+        -------
+        Dataset
+            validation dataset
+        """
+        dataset = self.get_dataset(
+            split="val",
+            transform=self.test_transform,
+        )
+        return dataset
+
+    def get_test_dataset(self) -> Dataset:
+        """Call self.get_dataset to return the test dataset.
+
+        Returns
+        -------
+        Dataset
+            test dataset
+        """
+        dataset = self.get_dataset(
+            split="test",
+            transform=self.test_transform,
+        )
+        return dataset
+
+
+    def setup(self, stage: Optional[str] = None) -> None:
+        """Register the correct datasets to the class attributes.
+
+        Depending on the trainer's stage, this method will retrieve
+        the train, val or test dataset and register it as a class
+        attribute. The "predict" stage calls for the test dataset.
+
+        Parameters
+        ----------
+        stage : Optional[str], optional
+            trainer's stage, by default None (train)
+        """
+        if stage in (None, "fit"):
+            self.dataset_train = self.get_train_dataset()
+            self.dataset_val = self.get_val_dataset()
+
+            self.train_sampler = RandomBatchGeoSampler(self.dataset_train, size=self.patch_size, batch_size=self.batch_size, length=10000)
+            self.val_sampler = GridGeoSampler(self.dataset_val, size=self.patch_size, stride=self.patch_size)
+
+        if stage == "test":
+            self.dataset_test = self.get_test_dataset()
+            self.test_sampler = GridGeoSampler(self.dataset_test, size=self.patch_size, stride=self.patch_size)
+
+        if stage == "predict":
+            self.dataset_predict = self.get_test_dataset()
+            self.predict_sampler = GridGeoSampler(self.dataset_predict, size=self.patch_size, stride=self.patch_size)
+
+    def train_dataloader(self) -> DataLoader:
+        """Return train dataloader instantiated with class attributes.
+
+        Returns
+        -------
+        DataLoader
+            train dataloader
+        """
+        dataloader = DataLoader(
+            self.dataset_train,
+            sampler=train_sampler,
+            batch_size=self.train_batch_size,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
+            shuffle=True,
+        )
+        return dataloader
+
+    def val_dataloader(self) -> DataLoader:
+        """Return validation dataloader instantiated with class attributes.
+
+        Returns
+        -------
+        DataLoader
+            Validation dataloader
+        """
+        dataloader = DataLoader(
+            self.dataset_val,
+            sampler=self.val_sampler,
+            batch_size=self.inference_batch_size,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
+        )
+        return dataloader
+
+    def test_dataloader(self) -> DataLoader:
+        """Return test dataloader instantiated with class attributes.
+
+        Returns
+        -------
+        DataLoader
+            test dataloader
+        """
+        dataloader = DataLoader(
+            self.dataset_test,
+            sampler=self.test_sampler,
+            batch_size=self.inference_batch_size,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
+        )
+        return dataloader
+
+    def predict_dataloader(self) -> DataLoader:
+        """Return predict dataloader instantiated with class attributes.
+
+        Returns
+        -------
+        DataLoader
+            predict dataloader
+        """
+        dataloader = DataLoader(
+            self.dataset_predict,
+            sampler=self.predict_sampler,
+            batch_size=self.inference_batch_size,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
+        )
+        return dataloader
