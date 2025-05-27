fe0977d04ff74d914766d7bb3528c04e98884918
Isabelle Mornard <mornardisabelle@gmail.com>
2025-05-27 09:52:39 +0200

    Use RegressionSystem from malpolon

--- examples/poverty/cnn_on_ms_poverty.py
+++ examples/poverty/cnn_on_ms_poverty.py
@@ -29,67 +29,67 @@ import torchmetrics.functional as Fmetrics
 from poverty_dataset import PovertyDataModule
 from malpolon.logging import Summary
 from malpolon.models.utils import check_metric, check_model, check_loss
-from malpolon.models.standard_prediction_systems import GenericPredictionSystem
+from malpolon.models.standard_prediction_systems import RegressionSystem
 
 import warnings
 from rasterio.errors import NotGeoreferencedWarning
 
 warnings.filterwarnings("ignore", category=NotGeoreferencedWarning)
 
-class RegressionSystem(GenericPredictionSystem):
-    """Regression task class."""
-    def __init__(
-        self,
-        model: Union[torch.nn.Module, Mapping],
-        loss: Union[torch.nn.modules.loss._Loss, str],
-        optimizer: Union[torch.nn.Module, Mapping] = None,
-        lr: float = 1e-2,
-        weight_decay: float = 0,
-        metrics: Optional[dict[str, Callable]] = None,
-        loss_kwargs: Optional[dict] = {},
-    ):
-        """Class constructor.
-        Parameters
-        ----------
-        model : dict
-            model to use
-        loss : Union[torch.nn.modules.loss._Loss, str]
-            loss or string from the predifined LOSS_CALLABLES.
-        optimizer : Union[torch.nn.Module, Mapping]
-            optional custom optimizer to use for training
-        lr : float
-            learning rate
-        weight_decay : float
-            weight decay
-        metrics : dict
-            dictionnary containing the metrics to compute.
-            Keys must match metrics' names and have a subkey with each
-            metric's functional methods as value. This subkey is either
-            created from the `malpolon.models.utils.FMETRICS_CALLABLES`
-            constant or supplied, by the user directly.
-        loss_kwargs: Optional[dict] = {}
-            Arguments to be passed to loss constructor.
-        """
-
-        metrics = check_metric(metrics)
-
-        self.lr = lr
-        self.weight_decay = weight_decay
-
-        model = check_model(model)
-
-        if optimizer is None:
-            print(f'[INFO] No optimizer provided: using AdamW with lr={lr}, weight_decay={weight_decay}')
-            optimizer = torch.optim.AdamW(
-                model.parameters(),
-                lr=self.lr,
-                weight_decay=self.weight_decay
-            )
-
-
-        loss = check_loss(loss)
-
-        super().__init__(model, loss, optimizer, metrics=metrics)
+# class RegressionSystem(GenericPredictionSystem):
+#     """Regression task class."""
+#     def __init__(
+#         self,
+#         model: Union[torch.nn.Module, Mapping],
+#         loss: Union[torch.nn.modules.loss._Loss, str],
+#         optimizer: Union[torch.nn.Module, Mapping] = None,
+#         lr: float = 1e-2,
+#         weight_decay: float = 0,
+#         metrics: Optional[dict[str, Callable]] = None,
+#         loss_kwargs: Optional[dict] = {},
+#     ):
+#         """Class constructor.
+#         Parameters
+#         ----------
+#         model : dict
+#             model to use
+#         loss : Union[torch.nn.modules.loss._Loss, str]
+#             loss or string from the predifined LOSS_CALLABLES.
+#         optimizer : Union[torch.nn.Module, Mapping]
+#             optional custom optimizer to use for training
+#         lr : float
+#             learning rate
+#         weight_decay : float
+#             weight decay
+#         metrics : dict
+#             dictionnary containing the metrics to compute.
+#             Keys must match metrics' names and have a subkey with each
+#             metric's functional methods as value. This subkey is either
+#             created from the `malpolon.models.utils.FMETRICS_CALLABLES`
+#             constant or supplied, by the user directly.
+#         loss_kwargs: Optional[dict] = {}
+#             Arguments to be passed to loss constructor.
+#         """
+#
+#         metrics = check_metric(metrics)
+#
+#         self.lr = lr
+#         self.weight_decay = weight_decay
+#
+#         model = check_model(model)
+#
+#         if optimizer is None:
+#             print(f'[INFO] No optimizer provided: using AdamW with lr={lr}, weight_decay={weight_decay}')
+#             optimizer = torch.optim.AdamW(
+#                 model.parameters(),
+#                 lr=self.lr,
+#                 weight_decay=self.weight_decay
+#             )
+#
+#
+#         loss = check_loss(loss)
+#
+#         super().__init__(model, loss, optimizer, metrics=metrics)
 
 
 @hydra.main(version_base="1.3", config_path="config", config_name="cnn_on_ms_torchgeo_config")
