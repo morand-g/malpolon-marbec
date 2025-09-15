d3115b6ab160f3b561d0d09ea04ecf578ab655c6
Gaetan Morand <gaetan.morand@umontpellier.fr>
2025-09-15 15:31:19 +0200

    modified:   config/rls_aus_reg.yaml 	modified:   ../../malpolon/data/data_module.py

--- malpolon/data/data_module.py
+++ malpolon/data/data_module.py
@@ -435,7 +435,8 @@ class RLSDataset(Dataset):
 
         if species_subsample is not None:
             self.species = species_subsample
-        
+
+        print(len(self.species), num_classes)
         assert len(self.species) == num_classes
 
         if self.training:
