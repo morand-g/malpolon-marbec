7c33f2c1255ea59fc933f2faef0ff4c288245f27
Gaetan Morand <gaetan.morand@umontpellier.fr>
2025-11-10 16:29:31 +0100

    modified:   config/rls_aus_binned.yaml 	modified:   launch.sh

--- examples/RLS/launch.sh
+++ examples/RLS/launch.sh
@@ -5,7 +5,7 @@
 #SBATCH --mem=32G
 #SBATCH --gres=gpu:1
 ###SBATCH --begin=now+1hour
-#SBATCH --dependency=afterany:13000
+#SBATCH --dependency=afterany:13098
 #SBATCH -o malpolon_rls.out
 #SBATCH -e malpolon_rls.err
 
