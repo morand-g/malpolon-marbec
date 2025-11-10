3e4b1bf3bd996e6c43bf25d594a6f1ae6d38c28f
Gaetan Morand <gaetan.morand@umontpellier.fr>
2025-11-10 11:51:14 +0100

    modified:   config/rls_aus_binned.yaml 	modified:   ../../malpolon/models/custom_models.py

--- malpolon/models/custom_models.py
+++ malpolon/models/custom_models.py
@@ -102,7 +102,7 @@ class MultiModalModel(nn.Module):
             elif aggregator == 'MLP':
 
                 self.aggregator_model =  torchvision.ops.MLP(in_channels = sum([x.in_features for x in linears]),
-                                                        hidden_channels = [256, linears[0].out_features],
+                                                        hidden_channels = [linears[0].out_features // 4, linears[0].out_features],
                                                         dropout = 0.1)
 
 
