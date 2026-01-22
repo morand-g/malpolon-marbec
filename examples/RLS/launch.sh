cfe92ca8cd9de51ce9184506af6220368d885507
Gaetan Morand <gaetan.morand@umontpellier.fr>
2026-01-22 16:49:18 +0100

    modified:   config/rls_aus_binned.yaml 	modified:   config/rls_aus_reg.yaml 	modified:   launch.sh 	modified:   rls_aus_binned.py

--- examples/RLS/launch.sh
+++ examples/RLS/launch.sh
@@ -4,11 +4,11 @@
 #SBATCH --job-name=malpolon_rls
 #SBATCH --mem=128G
 #SBATCH --gres=gpu:1
-#SBATCH --dependency=afterany:15651
+#SBATCH --dependency=afterany:15772
 #SBATCH --cpus-per-task=4
 #SBATCH -o malpolon_rls.out
 #SBATCH -e malpolon_rls.err
 
 source /home/gmorand/venvs/deepsdm2/bin/activate
-python rls_aus_binned.py -cn "rls_aus_binned_pa"
+python rls_aus_binned.py -cn "rls_aus_binned"
 #nsys profile --trace=cuda,nvtx,osrt --force-overwrite true -o profiling_test python rls_aus_binned.py -cn "rls_aus_fm"
