881abc55b826eb34ea47d4391e35a0d722a541c4
Gaetan Morand <gaetan.morand@umontpellier.fr>
2026-03-02 17:39:32 +0100

    modified:   config/rls_aus_reg_eco.yaml 	modified:   launchreg.sh

--- examples/RLS/launchreg.sh
+++ examples/RLS/launchreg.sh
@@ -4,11 +4,11 @@
 #SBATCH --job-name=malpolon_rls_reg
 #SBATCH --mem=128G
 #SBATCH --gres=gpu:1
-#SBATCH --dependency=afterany:16707
+#SBATCH --dependency=afterany:16713
 #SBATCH --cpus-per-task=4
 #SBATCH --ntasks-per-node=2
 #SBATCH -o malpolon_rls_reg.out
 #SBATCH -e malpolon_rls_reg.err
 
 source /home/gmorand/venvs/deepsdm3/bin/activate
-python rls_aus_reg.py -cn "rls_aus_reg"
+python rls_aus_reg.py -cn "rls_aus_reg_eco"
